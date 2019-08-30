e38fcd0d4fb1c3df4ceedd656452074b307802e5
Michael Gates <michaelgatesdev@gmail.com>
2019-08-30 08:33:35 -0400

    Fixed the unit tests to be proper

--- src/__tests__/file-utils.test.ts
+++ src/__tests__/file-utils.test.ts
@@ -4,22 +4,27 @@ const dirPath = "_my_cool_directory_";
 test("should create a directory", async () => {
     // create dir
     const createRes: boolean = await FileUtils.createDirectory(dirPath);
-    expect(createRes);
+    expect(createRes).toBe(true);
+});
+test("should check if directory is empty", async () => {
+    // check empty dir
+    const emptyRes: boolean = await FileUtils.isDirectoryEmpty(dirPath);
+    expect(emptyRes).toBe(true);
 });
 test("should delete a directory", async () => {
     // delete dir
     const delRes: boolean = await FileUtils.delete(dirPath);
-    expect(delRes);
+    expect(delRes).toBe(true);
 });
 
 const jsonPath = "_test.json";
 test("should write a json file", async () => {
     // write json
     const writeRes: boolean = await FileUtils.writeJSON(jsonPath, { name: "test", description: "hello world" });
-    expect(writeRes);
+    expect(writeRes).toBe(true);
 });
 test("should delete the just-created json file", async () => {
     // delete json
     const delRes: boolean = await FileUtils.delete(jsonPath);
-    expect(delRes);
+    expect(delRes).toBe(true);
 });
