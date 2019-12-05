093666b8b201d72d98bf06b17a13266c0df99aa5
Michael Gates <michaelgatesdev@gmail.com>
2019-12-05 14:29:14 -0500

    Add function to strip non-alphanumeric characters and also update docs and tests

--- src/__tests__/string-utils.test.ts
+++ src/__tests__/string-utils.test.ts
@@ -1,5 +1,17 @@
 import { StringUtils } from "../string-utils";
 
+test("detect blank string", () => {
+    const blank = "";
+    const alsoBlank = " ";
+    const blankUndef = undefined;
+    const blankNull = null;
+
+    expect(StringUtils.isBlank(blank)).toBe(true);
+    expect(StringUtils.isBlank(alsoBlank)).toBe(true);
+    expect(StringUtils.isBlank(blankUndef)).toBe(true);
+    expect(StringUtils.isBlank(blankNull)).toBe(true);
+});
+
 test("should split up a string", () => {
     const compressed = "ThisIsACompressedString";
     const split = StringUtils.splitCompressedTitle(compressed);
@@ -7,14 +19,52 @@ test("should split up a string", () => {
     expect(split.length).not.toBe(1);
 });
 
-test("should pad a young month", () => {
+test("should capitalize the first letter of each word", () => {
+    const lower = "this is a string";
+    const capitalized = "This Is A String";
+    expect(StringUtils.capitalizeFirstLetter(lower)).toBe(capitalized);
+});
+
+test("should parse boolean", () => {
+    // clearly true
+    expect(StringUtils.parseBoolean("true")).toBe(true);
+    expect(StringUtils.parseBoolean("on")).toBe(true);
+    expect(StringUtils.parseBoolean("YES")).toBe(true);
+    expect(StringUtils.parseBoolean("Enabled")).toBe(true);
+    expect(StringUtils.parseBoolean("active")).toBe(true);
+    // false
+    expect(StringUtils.parseBoolean("false")).toBe(false);
+    // anything else (also false)
+    expect(StringUtils.parseBoolean("asdf")).toBe(false);
+});
+
+test("should pad an early month", () => {
     const month = 9;
     expect(StringUtils.pad(`${month}`, "0", 2).length).toBe(2);
     expect(StringUtils.pad(`${month}`, "0", 2)).toBe(`0${month}`);
 });
 
-test("should not pad an old month", () => {
+test("should not pad a late month", () => {
     const month = 12;
     expect(StringUtils.pad(`${month}`, "0", 2).length).toBe(2);
     expect(StringUtils.pad(`${month}`, "0", 2)).toBe(`${month}`);
+});
+
+test("should internalize strings", () => {
+    const standard = "this is a standard string";
+    expect(StringUtils.internalize(standard).includes(" ")).toBe(false);
+    const crazy = "this !@# IS #!(*@# a__ crazy string";
+    expect(StringUtils.internalize(crazy).includes(" ")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("!")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("@")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("#")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("$")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("%")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("_")).toBe(false);
+    expect(StringUtils.internalize(crazy).includes("-")).toBe(true); // still want our dashes
+});
+
+test("should strip all non-alphanumeric characters from a string", () => {
+    const str = "Is this a question?";
+    expect(StringUtils.stripNonAlphanumeric(str).includes("?")).toBe(false);
 });
--- src/string-utils.ts
+++ src/string-utils.ts
@@ -8,8 +8,8 @@ export class StringUtils {
      * Checks if a string is blank, null, or undefined
      * @param str The string to test
      */
-    public static isBlank(str: string): boolean {
-        return (!str || /^\s*$/.test(str));
+    public static isBlank(str: string | null | undefined): boolean {
+        return (str === undefined || str === null || /^\s*$/.test(str));
     }
 
     /**
@@ -37,15 +37,41 @@ export class StringUtils {
             .join(" ");
     }
 
+    /**
+     * Parses a boolean and returns true if it matches any of the following:
+     * true, on, yes, enabled, active, 1
+     * @param text string to parse
+     */
     public static parseBoolean(text: string): boolean {
         text = text.toLowerCase().trim();
         return TRUE_STRINGS.includes(text);
     }
 
+    /**
+     * Strips all non-alphanumeric characters from a string and replaces all spaces in a string with dashes (-)
+     * @param str string to internalize
+     */
     public static internalize(str: string): string {
-        return str.toLowerCase().replace(/\s/g, "-");
+        return this.stripNonAlphanumeric(str.toLowerCase(), true).replace(/[ ]/g, "-");
     }
 
+    /**
+     * Strips all non-alphanumeric characters from a string (e.g. !@#$%&*()_=+)
+     * @param text string to strip from
+     * @param allowSpaces if spaces should not be stripped
+     */
+    public static stripNonAlphanumeric(text: string, allowSpaces = false): string {
+        const pattern = allowSpaces ? /[^a-zA-Z0-9 ]/g : /[^a-zA-Z0-9]/g;
+        text = text.replace(pattern, "");
+        return text;
+    }
+
+    /**
+     * 
+     * @param str the string to pad
+     * @param padStr the string to pad the string with
+     * @param length the expected length of the resulting string after padding
+     */
     public static pad(str: string, padStr: string, length: number): string {
         let result = str;
         while (result.length < length) {
